2f78340504f126c9ef0c5c4a2f6993302bae52ea
3389 <914930238@qq.com>
2022-02-13 21:31:25 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -9,20 +9,20 @@
 
 <script>
 export default{
-  mounted: function () {
-    // 页面开始加载时修改font-size
-    var html = document.getElementsByTagName("html")[0];
-    var oWidth =
-      document.body.clientWidth || document.documentElement.clientWidth;
-    // 这里的1920是指设计图的大小,自己根据实际情况改变
-    html.style.fontSize = (oWidth / 1920) * 100 + "px";
-    console.log("rem:", html.style.fontSize);
-  }
+  // mounted: function () {
+  //   // 页面开始加载时修改font-size
+  //   var html = document.getElementsByTagName("html")[0];
+  //   var oWidth =
+  //     document.body.clientWidth || document.documentElement.clientWidth;
+  //   // 这里的1920是指设计图的大小,自己根据实际情况改变
+  //   html.style.fontSize = (oWidth / 1920) * 100 + "px";
+  //   console.log("rem:", html.style.fontSize);
+  // }
 }
 </script>
 <style lang="scss">
 html,body{
-  background: #34495e;
+  background: #505253;
 }
 @media screen and (max-width: 3840) {
   body,
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,174 +1,43 @@
 <template>
-  <div class="main">
+  <div class="flex">
     <div class="title">
       <span>车辆布控实时分析</span>
       <div class="line"></div>
     </div>
-    <div class="left">
-      <div class="time">2022/2/7</div>
-      <div class="streamer">
-        <div class="line"></div>
+
+    <div class="box">
+      <div class="one">
+        <img src="../assets/img/2.jpg" alt="" />
       </div>
-      <div class="imgage">
-        <!-- <img src="../assets/img/2.jpg" alt="" /> -->
+      <div class="item2 one"></div>
+      <div class="item3 one">
+        <img src="../assets/img/2.jpg" alt="" />
+      </div>
+      <div class="item4 one">
+           <div class="wrap active">
+    <img src="../assets/img/anomalies/今日识别人数icon@2x.png" alt="">
+    <span>布控数量<br>0个.</span>
+</div>
+   <div class="wrap">
+    <img src="../assets/img/anomalies/今日识别人数icon@2x.png" alt="">
+    <span>今日识别车辆<br>0个.</span>
+</div>
       </div>
     </div>
-
-    <div class="mains">
-      <div ref="cartes" id="carte"></div>
-    </div>
-    <div class="font">匹配度</div>
-
-    <div class="imgs">
-      <img src="../assets/img/2.jpg" alt="" />
-    </div>
-    <div class="number">
-      <img src="../assets/img/anomalies/今日识别人数icon@2x.png" alt="" />
-    </div>
-    <div class="fonts">布控数量</div>
-    <div class="count">0个</div>
-
-    <div class="numbers">
-      <img src="../assets/img/anomalies/今日识别车辆icon@2x.png" alt="" />
-    </div>
-
-    <div class="fontss">今日识别车辆</div>
-    <div class="counts">0个</div>
   </div>
 </template>
 
 <script>
-import echarts from 'echarts'
-export default {
-  data() {
-    return {}
-  },
-  mounted() {
-    this.draw()
-  },
-  methods: {
-    draw() {
-      let myChart = echarts.init(document.getElementById('carte'))
-      let option = null
-      option = {
-        title: [
-          {
-            x: 'center',
-            top: '55%',
-            textStyle: {}
-          },
-          {
-            text: '23%',
-            x: 'center',
-            top: '38%',
-            textStyle: {
-              fontSize: '12',
-              color: '#FFFFFF'
-            }
-          }
-        ],
-        polar: {
-          radius: ['42%', '52%'],
-          center: ['50%', '50%']
-        },
-        angleAxis: {
-          max: 100,
-          show: false
-        },
-        radiusAxis: {
-          type: 'category',
-          show: true,
-          axisLabel: {
-            show: false
-          },
-          axisLine: {
-            show: false
-          },
-          axisTick: {
-            show: false
-          }
-        },
-        series: [
-          {
-            name: '',
-            type: 'bar',
-            roundCap: true,
-            barWidth: 90,
-            showBackground: true,
-            backgroundStyle: {
-              color: 'rgba(66, 66, 66, .3)'
-            },
-            //
-            data: [50],
-            coordinateSystem: 'polar',
-            itemStyle: {
-              normal: {
-                color: new echarts.graphic.LinearGradient(0, 1, 0, 0, [
-                  {
-                    offset: 0,
-                    color: 'rgba(80, 225, 255, 1)'
-                  },
-                  {
-                    offset: 1,
-                    color: 'rgba(80, 225, 255, 1)'
-                  }
-                ])
-              }
-            }
-          },
-          {
-            name: '',
-            type: 'pie',
-            startAngle: 80,
-            radius: ['56%'],
-            hoverAnimation: false,
-            center: ['50%', '50%'],
-            itemStyle: {
-              color: 'rgba(66, 66, 66, .1)',
-              borderWidth: 1,
-              borderColor: '#34495e'
-            }
-          },
-          {
-            name: '',
-            type: 'pie',
-            startAngle: 80,
-            radius: ['38%'],
-            hoverAnimation: false,
-            center: ['50%', '50%'],
-            itemStyle: {
-              color: 'rgba(66, 66, 66, .1)',
-              borderWidth: 1,
-              borderColor: '#34495e'
-            }
-            // data: [75]
-          }
-        ]
-      }
-
-      let resizeFn = () => {
-        myChart.resize()
-      }
-      window.addEventListener('resize', resizeFn)
-      this.$once('beforeDestroy', () => {
-        window.removeEventListener('resize', resizeFn)
-      })
-      myChart.setOption(option)
-    }
-  }
-}
+export default {};
 </script>
 
 <style lang="scss" scoped>
-* {
-  margin: 0;
-  padding: 0;
-}
-.main {
-  position: relative;
-  width: 620px;
-  height: 314px;
-  background: #000;
+.flex {
+  position: absolute;
+  width: 310px;
+  // display: flex;
+  height: 152px;
+  // display: inline-block;
   .title {
     width: 100%;
     height: 2.96vh;
@@ -203,187 +72,47 @@ export default {
       opacity: 1;
     }
   }
-  .left {
+      .wrap {
+/* 可无需设置高度，靠图片高度 或者 文字的上下padding撑开高度 */
+    // border: 1px solid;
+    width:106%;
+  
+}
+.wrap span{
+    display: inline-block;
+    vertical-align: middle;
+    padding: 10px 0; /* 撑开高度 */
+}
+.wrap img{
+    width:30%;
+    height: auto;
+    vertical-align: middle;
+}
+}
+.box {
+  width: 100%;
+  display: flex;
+  height: 100%;
+  .one {
+    // flex: 1;
     width: 20.6%;
     height: 55.9%;
-    margin-left: 0.2vh;
-    background: url('../assets/img/2.jpg') no-repeat center center;
-    background-size: 90% 90%;
-    // border: 1px solid red;
-    .time {
-      position: absolute;
-      bottom: 0.0505rem;
-      right: calc(100% - 0.55rem);
-      color: #fff;
-      font-size: 0.1212rem;
-    }
+   
   }
-
-  // streamer
-  .streamer {
-    position: relative;
+  .one:last-child {
+    width: 45%;
+    height: 55.9%;
+  }
+  img {
     width: 100%;
     height: 100%;
-    background: linear-gradient(to left, #058580, #1289a7) left top no-repeat,
-      linear-gradient(to bottom, #058580, #1289a7) left top no-repeat,
-      linear-gradient(to left, #058580, #1289a7) right top no-repeat,
-      linear-gradient(to bottom, #058580, #1289a7) right top no-repeat,
-      linear-gradient(to left, #058580, #1289a7) left bottom no-repeat,
-      linear-gradient(to bottom, #058580, #1289a7) left bottom no-repeat,
-      linear-gradient(to left, #058580, #1289a7) right bottom no-repeat,
-      linear-gradient(to left, #058580, #1289a7) right bottom no-repeat;
-    background-size: 2px 10px, 10px 1px, 1px 10px, 10px 1px;
-  }
-  .streamer .line {
-    width: 100%;
-    position: absolute;
-    height: calc(100% - 2px);
-    background: linear-gradient(
-      180deg,
-      rgba(18, 170, 48, 0) 43%,
-      rgb(15, 15, 15) 211%
-    );
-    transform: translateY(-100%);
-    animation: radar-beam 2s infinite;
-    transition: all 1s linear;
-    border-bottom: 1px solid #0abde3;
-    animation-timing-function: cubic-bezier(0.53, 0.43, 0.99);
-    animation-delay: 1.7s;
-  }
-  @keyframes radar-beam {
-    from {
-      top: 0px;
-    }
-    to {
-      top: 100%;
-    }
-  }
-  .streamer::before,
-  .streamer::after {
-    position: absolute;
-    content: '';
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
-    border-radius: 5px;
-    // border-top-width: 5px;
-    border: 1px solid #005260;
-    border-image: linear-gradient(#00cec9, #74b9ff, #00b894) 30 30;
-    transition: all 0.5s;
-    animation: clippath 3s infinite linear;
-  }
-  .streamer::after {
-    animation: clippath 3s infinite -1.5s linear;
   }
 
-  @keyframes clippath {
-    0%,
-    100% {
-      clip-path: inset(0 0 98% 0);
-    }
-    25% {
-      clip-path: inset(0 98% 0 0);
-    }
-    50% {
-      clip-path: inset(98% 0 0 0);
-    }
-    75% {
-      clip-path: inset(0 0 0 98%);
-    }
-  }
-  .mains {
-    width: 2.1vw;
-    height: 3.7vh;
-    display: flex;
-    text-align: center;
-    // position: relative;
-    margin: 0 auto;
-    position: absolute;
-    top: 40.4%;
-    left: 22.5%;
-    #carte {
-      width: 2.1vw;
-      height: 3.7vh;
-    }
-  }
-  .font {
-    color: #fff;
-    font-size: 0.1414rem;
-    position: absolute;
-    // left: 37.1%;
-    top: 10.5vh;
-    left: 23.5%;
-    text-align: center;
-  }
-
-  .imgs {
-    position: absolute;
-    left: 39.34%;
-    top: 16.37%;
-    width: 20.6%;
-    height: 55.9%;
-  }
-  .number {
-    position: absolute;
-    left: 61.84%;
-    top: -16.1%;
-    width: 1.9vw;
-    height: 3.3vh;
-  }
-  .fonts {
-    position: absolute;
-    top: 27%;
-    left: 74.88%;
-    // width: 5.2vw;
-    // width: auto;
-    font-size: 0.1212rem;
-    color: #fff;
-  }
-
-  .fontss {
-    position: absolute;
-    top: 60%;
-    left: 74.88%;
-    // width: 5.2vw;
-    // width: auto;
-    font-size: 0.1212rem;
-    color: #fff;
-  }
-  .counts {
-    position: absolute;
-    top: 73%;
-    left: 74.88%;
-    font-size: 0.1212rem;
-    color: #fff;
-  }
-  .count {
-    position: absolute;
-    top: 39%;
-    left: 74.88%;
-    // width: 5.2vw;
-    // width: auto;
-    font-size: 0.1212rem;
-    color: #fff;
-  }
-  .numbers {
-    position: absolute;
-    left: 61.84%;
-    top: 16.42%;
-    width: 1.9vw;
-    height: 3.3vh;
-  }
-  .imgage {
-    position: absolute;
-    left: 1%;
-    top: 15.37%;
-    z-index: 1;
-    width: 19.6%;
-    height: 50.9%;
-  }
-     img {
-      width: 100%;
-      height: 100%;
-    }
+.active{
+  margin-bottom: 20px;
 }
+
+
+}
+
 </style>
